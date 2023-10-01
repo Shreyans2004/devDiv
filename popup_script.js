ba1eb769281ecb4e2e02a1f65d0cbd1e759e89bb
Shreyans Katariya <128773151+Shreyans2004@users.noreply.github.com>
2023-10-01 17:35:45 +0530

    commented some parts

--- popup_script.js
+++ popup_script.js
@@ -1,9 +1,14 @@
+//GRABBING IMPORTANT IDS
 const devdiv = document.getElementById("devdiv");
 const devdiv1 = document.getElementById("devdiv1");
 const devdiv2 = document.getElementById("devdiv2");
 const devdiv3 = document.getElementById("devdiv3");
 const devdiv4 = document.getElementById("devdiv4");
+
+//LINK FOR devDivweb
 var q="https://devdiv-web.netlify.app/";
+
+// ADDING EVENT LISTENERS
 devdiv.addEventListener("click", function () {
     let params={
         active:true,
@@ -24,7 +29,7 @@ devdiv1.addEventListener("click", function () {
     }
     chrome.tabs.query(params,gotTabs);
   function gotTabs(tabs) {
- 
+      //MAKING THE ELEMENT'S DISPLAY AS BLOCK TO ALLOW THE TABLE CONTENTS TO BE VISIBLE
     document.getElementById("span1").style.display="block";
     document.getElementById("span2").style.display="block";
     document.getElementById("span3").style.display="block";
@@ -33,6 +38,8 @@ devdiv1.addEventListener("click", function () {
     document.getElementById("span6").style.display="block";
     document.getElementById("span7").style.display="block";
     document.getElementById("table_1").style.display="block";
+
+      //GENERATING RANDOM COLORS 
     var c1=`#${Math.floor(Math.random()*16777215).toString(16)}`;
     var c2=`#${Math.floor(Math.random()*16777215).toString(16)}`;
     var c3=`#${Math.floor(Math.random()*16777215).toString(16)}`;
@@ -40,14 +47,17 @@ devdiv1.addEventListener("click", function () {
     var c5=`#${Math.floor(Math.random()*16777215).toString(16)}`;
     var c6=`#${Math.floor(Math.random()*16777215).toString(16)}`;
     var c7=`#${Math.floor(Math.random()*16777215).toString(16)}`;
+
+      //ADDING COLOR NAMES TO TABLE
     document.getElementById("c1_1").innerHTML=`${c1}`;
     document.getElementById("c2_1").innerHTML=`${c2}`;
     document.getElementById("c3_1").innerHTML=`${c3}`;
     document.getElementById("c4_1").innerHTML=`${c4}`;
-    
     document.getElementById("c5_1").innerHTML=`${c5}`;
     document.getElementById("c6_1").innerHTML=`${c6}`;
     document.getElementById("c7_1").innerHTML=`${c7}`;
+
+      //ADDING COLORED DIV TO TABLE FOR COLOR VISIBILITY TO THE USER
     document.getElementById("c1_2").style.backgroundColor=`${c1}`;
     document.getElementById("c2_2").style.backgroundColor=`${c2}`;
     document.getElementById("c3_2").style.backgroundColor=`${c3}`;
@@ -70,6 +80,7 @@ devdiv1.addEventListener("click", function () {
     chrome.tabs.sendMessage(tabs[0].id, msg1);
   }
 });
+
 devdiv2.addEventListener("click", function () {
   let params={
       active:true,
@@ -84,6 +95,7 @@ function gotTabs(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, msg);
 }
 });
+
 devdiv3.addEventListener("click", function () {
   let params={
       active:true,
@@ -98,6 +110,7 @@ function gotTabs(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, msg);
 }
 });
+
 devdiv4.addEventListener("click", function () {
   let params={
       active:true,
@@ -107,7 +120,7 @@ devdiv4.addEventListener("click", function () {
 function gotTabs(tabs) {
   let msg = {
     txt: "hello",
-    link:q,
+    link:q,                            //ADDING q TO LINK 
     p:5
   };
   chrome.tabs.sendMessage(tabs[0].id, msg);
